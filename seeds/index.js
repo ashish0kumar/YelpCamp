bc8e2ad7933307019dbe983b4bbbfb81902719ac
Ashish Kumar <ashishkumar901336@gmail.com>
2024-05-17 20:45:22 +0530

    Initial commit

---
+++ seeds/index.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Campground = require("../models/campground");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error!"));
+db.once("open", () => {
+    console.log("Database connected");
+})
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const camp = new Campground({
+            location: `${sample(cities).city}, ${sample(cities).state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
+
