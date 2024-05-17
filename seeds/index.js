0369bde1153080eea345519b84bca0d0a1e8ea83
Ashish Kumar <ashishkumar901336@gmail.com>
2024-05-17 21:24:51 +0530

    added images

--- seeds/index.js
+++ seeds/index.js
@@ -16,9 +16,13 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${sample(cities).city}, ${sample(cities).state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/483251",
+            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, fuga suscipit dolor in distinctio tempora id sit harum ipsa incidunt aperiam quam, ipsam provident quod libero exercitationem nam voluptas perferendis.",
+            price
         })
         await camp.save();
     }
@@ -27,4 +31,3 @@ const seedDB = async () => {
 seedDB().then(() => {
     mongoose.connection.close();
 })
-
