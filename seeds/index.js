5b7c0284e9779a872c517ae92b15bbaf4497f1f9
Ashish Kumar <ashishkumar901336@gmail.com>
2024-05-19 19:12:33 +0530

    random images from unspash api

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
+const axios = require("axios");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 
@@ -15,12 +16,14 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 30; i++) {
         const price = Math.floor(Math.random() * 20) + 10;
+        const res = await axios.get("https://api.unsplash.com/photos/random?client_id=tVnezep4QeEafTgUrQM2QVhBZiVpWIKwc_K0xChaj1k&collections=483251&count=1&orientation=landscape");
+        const imgURL = res.data[0].urls.raw;
         const camp = new Campground({
             location: `${sample(cities).city}, ${sample(cities).state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: "https://source.unsplash.com/collection/483251/1920x1080?orientation=landscape",
+            image: imgURL,
             description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, fuga suscipit dolor in distinctio tempora id sit harum ipsa incidunt aperiam quam, ipsam provident quod libero exercitationem nam voluptas perferendis.",
             price
         })
