4c63cd36ee3ce9c49e752a32e60808aabb1089ff
Ashish Kumar <ashishkumar901336@gmail.com>
2024-05-24 17:10:45 +0530

    added reviews display feature

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.post("/campgrounds", validateCampground, catchAsync(async (req, res) => {
 }));
 
 app.get("/campgrounds/:id", catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate("reviews");
     res.render("campgrounds/show", { campground });
 }));
 
