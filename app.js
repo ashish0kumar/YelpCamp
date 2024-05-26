b434c8357c1dd081ef71f22b8e7174d7b3d34cd5
Ashish Kumar <ashishkumar901336@gmail.com>
2024-05-26 22:59:00 +0530

    serving static assets

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
@@ -38,6 +39,7 @@ app.get("/", (req, res) => {
 app.all("*", (req, res, next) => {
     next(new ExpressError("Page Not Found!", 404));
 });
+
 
 
 //* Error handler
