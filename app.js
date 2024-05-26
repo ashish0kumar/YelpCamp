071aa470c2b3654c3b6f116f8f36ba57bcfe4dae
Ashish Kumar <ashishkumar901336@gmail.com>
2024-05-26 23:11:10 +0530

    configured session

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
+const session = require("express-session");
 
 const ExpressError = require("./utils/ExpressError");
 
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
+const { date } = require("joi");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
@@ -26,6 +28,20 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+
+const sessionConfig = {
+    secret: "thisshouldbeabettersecret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig));
 
 
 app.use("/campgrounds", campgrounds);
