b3b293631aa7266003a44d6c322f811f05857e61
Ashish Kumar <ashishkumar901336@gmail.com>
2024-05-29 00:20:06 +0530

    added logout btn when a user is logged in

--- app.js
+++ app.js
@@ -52,12 +52,6 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
-app.use((req, res, next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    next();
-});
-
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -65,6 +59,15 @@ passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+});
+
 
 app.use("/", userRoutes);
 app.use("/campgrounds", campgroundRoutes);
