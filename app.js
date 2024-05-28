682f6543e14d39dcdc16c5f0655e528a68ebb461
Ashish Kumar <ashishkumar901336@gmail.com>
2024-05-28 22:15:59 +0530

    configured passport

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
+
 const session = require("express-session");
 const flash = require("connect-flash");
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 const ExpressError = require("./utils/ExpressError");
 
@@ -50,6 +55,14 @@ app.use((req, res, next) => {
     res.locals.error = req.flash("error");
     next();
 });
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.use("/campgrounds", campgrounds);
