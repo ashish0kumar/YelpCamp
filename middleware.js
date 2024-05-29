a64e8b70a6d45cc084e55e3e68921acf9c7a3565
Ashish Kumar <ashishkumar901336@gmail.com>
2024-05-29 19:41:36 +0530

    added reviews permission and fixed reviews layout

--- middleware.js
+++ middleware.js
@@ -6,7 +6,7 @@ const Campground = require("./models/campground");
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl;
-        req.flash("error", "You must be signed in first");
+        req.flash("error", "You must log in first");
         return res.redirect("/login");
     }
     next();
