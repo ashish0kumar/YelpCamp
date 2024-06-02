f19a6d1ea8d6cd2bbed2aaa67fed0148e4fe504e
Ashish Kumar <ashishkumar901336@gmail.com>
2024-06-03 00:52:53 +0530

    added maps

---
+++ public/js/showPageMap.js
@@ -0,0 +1,18 @@
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 10, // starting zoom
+});
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 50 })
+            .setHTML(
+                `<h5>${campground.title}</h3>
+                <p>${campground.location}</p>`
+        )
+    )
+    .addTo(map);
--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
-const cities = require("./cities");
-const { places, descriptors } = require("./seedHelpers");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
@@ -13,12 +11,16 @@ db.once("open", () => {
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    await Campground.insertMany([
+    const campgrounds = [
         {
             title: 'Dusty River',
             price: 18,
             description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, fuga suscipit dolor in distinctio tempora id sit harum ipsa incidunt aperiam quam, ipsam provident quod libero exercitationem nam voluptas perferendis.',
-            location: 'Merced, Texas',
+            location: 'Yosemite National Park, California',
+            geometry: {
+                type: 'Point',
+                coordinates: [-119.538329, 37.865101]
+            },
             reviews: [],
             images: [
                 {
@@ -38,14 +40,18 @@ const seedDB = async () => {
                     filename: 'YelpCamp/o7a1s2q44r0owvwtgfmi',
                 }
             ],
-            author: '6656f72c841078ded5dabd6c',
+            author: '6656f72c841078ded5dabd6c', // Your user id
             __v: 3
         },
         {
             title: 'Diamond Flats',
             price: 28,
             description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, fuga suscipit dolor in distinctio tempora id sit harum ipsa incidunt aperiam quam, ipsam provident quod libero exercitationem nam voluptas perferendis.',
-            location: 'Prescott, New York',
+            location: 'Adirondack Park, New York',
+            geometry: {
+                type: 'Point',
+                coordinates: [-74.014095, 44.112318]
+            },
             reviews: [],
             images: [
                 {
@@ -57,14 +63,18 @@ const seedDB = async () => {
                     filename: 'YelpCamp/m3fe5n1rkd9dy9ei8noo',
                 }
             ],
-            author: '6656f72c841078ded5dabd6c',
+            author: '6656f72c841078ded5dabd6c', // Your user id
             __v: 0
         },
         {
             title: 'Maple Mule Camp',
             price: 22,
             description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, fuga suscipit dolor in distinctio tempora id sit harum ipsa incidunt aperiam quam, ipsam provident quod libero exercitationem nam voluptas perferendis.',
-            location: 'Alexandria, Wisconsin',
+            location: 'Devils Lake State Park, Wisconsin',
+            geometry: {
+                type: 'Point',
+                coordinates: [-89.733788, 43.422956]
+            },
             reviews: [],
             images: [
                 {
@@ -76,14 +86,18 @@ const seedDB = async () => {
                     filename: 'YelpCamp/elefehnuzaxtj2xvahzm',
                 }
             ],
-            author: '6656f72c841078ded5dabd6c',
+            author: '6656f72c841078ded5dabd6c', // Your user id
             __v: 0
         },
         {
             title: 'Bullfrog Springs',
             price: 17,
             description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, fuga suscipit dolor in distinctio tempora id sit harum ipsa incidunt aperiam quam, ipsam provident quod libero exercitationem nam voluptas perferendis.',
-            location: 'Middletown, Texas',
+            location: 'Big Bend National Park, Texas',
+            geometry: {
+                type: 'Point',
+                coordinates: [-103.250106, 29.127486]
+            },
             reviews: [],
             images: [
                 {
@@ -95,14 +109,18 @@ const seedDB = async () => {
                     filename: 'YelpCamp/ke2d4haa9ghryfjjormn',
                 }
             ],
-            author: '6656f72c841078ded5dabd6c',
+            author: '6656f72c841078ded5dabd6c', // Your user id
             __v: 0
         },
         {
             title: 'Misty Backcountry',
             price: 18,
             description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, fuga suscipit dolor in distinctio tempora id sit harum ipsa incidunt aperiam quam, ipsam provident quod libero exercitationem nam voluptas perferendis.',
-            location: 'Warner Robins, Texas',
+            location: 'Great Smoky Mountains National Park, Tennessee',
+            geometry: {
+                type: 'Point',
+                coordinates: [-83.507020, 35.653200]
+            },
             reviews: [],
             images: [
                 {
@@ -114,14 +132,18 @@ const seedDB = async () => {
                     filename: 'YelpCamp/rqod45apyvi1a0rbjdui',
                 }
             ],
-            author: '6656f72c841078ded5dabd6c',
+            author: '6656f72c841078ded5dabd6c', // Your user id
             __v: 0
         },
         {
             title: 'Sea Creekside',
             price: 29,
             description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, fuga suscipit dolor in distinctio tempora id sit harum ipsa incidunt aperiam quam, ipsam provident quod libero exercitationem nam voluptas perferendis.',
-            location: 'Altamonte Springs, Wisconsin',
+            location: 'Olympic National Park, Washington',
+            geometry: {
+                type: 'Point',
+                coordinates: [-123.498642, 47.802107]
+            },
             reviews: [],
             images: [
                 {
@@ -129,77 +151,89 @@ const seedDB = async () => {
                     filename: 'YelpCamp/ij444saowtmy31aqj0jb',
                 },
                 {
-                    url: 'https://res.cloudinary.com/dwtslthej/image/upload/v1717316281/YelpCamp/zrubbgtuwi8ksvprt0vj.jpg',
-                    filename: 'YelpCamp/zrubbgtuwi8ksvprt0vj',
+                    url: 'https://res.cloudinary.com/dwtslthej/image/upload/v1717316404/YelpCamp/a1dbe63pxqqtnqyinrkc.jpg',
+                    filename: 'YelpCamp/a1dbe63pxqqtnqyinrkc',
                 }
             ],
-            author: '6656f72c841078ded5dabd6c',
+            author: '6656f72c841078ded5dabd6c', // Your user id
             __v: 0
         },
         {
-            title: 'Cascade Hunting Camp',
-            price: 13,
+            title: 'Rocky Point',
+            price: 23,
             description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, fuga suscipit dolor in distinctio tempora id sit harum ipsa incidunt aperiam quam, ipsam provident quod libero exercitationem nam voluptas perferendis.',
-            location: 'Cedar Hill, Utah',
+            location: 'Rocky Mountain National Park, Colorado',
+            geometry: {
+                type: 'Point',
+                coordinates: [-105.683639, 40.342793]
+            },
             reviews: [],
             images: [
                 {
-                    url: 'https://res.cloudinary.com/dwtslthej/image/upload/v1717316404/YelpCamp/a1dbe63pxqqtnqyinrkc.jpg',
-                    filename: 'YelpCamp/a1dbe63pxqqtnqyinrkc',
+                    url: 'https://res.cloudinary.com/dwtslthej/image/upload/v1717316281/YelpCamp/zrubbgtuwi8ksvprt0vj.jpg',
+                    filename: 'YelpCamp/zrubbgtuwi8ksvprt0vj',
                 },
                 {
                     url: 'https://res.cloudinary.com/dwtslthej/image/upload/v1717316404/YelpCamp/fywfr8k3oid0lcmyuxsj.jpg',
                     filename: 'YelpCamp/fywfr8k3oid0lcmyuxsj',
                 }
             ],
-            author: '6656f72c841078ded5dabd6c',
+            author: '6656f72c841078ded5dabd6c', // Your user id
             __v: 0
         },
         {
-            title: 'Diamond Sands',
-            price: 25,
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, fuga suscipit dolor in distinctio tempora id sit harum ipsa incidunt aperiam quam, ipsam provident quod libero exercitationem nam voluptas perferendis.',
-            location: 'Mesquite, New York',
-            reviews: [],
-            images: [
+            "title": "Canyon Oasis",
+            "price": 20,
+            "description": "A serene oasis in the heart of the canyon, perfect for a peaceful retreat.",
+            "location": "Grand Canyon National Park, Arizona",
+            "reviews": [],
+            "images": [
                 {
-                    url: 'https://res.cloudinary.com/dwtslthej/image/upload/v1717316516/YelpCamp/casxjgqi9oupmti23iw2.jpg',
-                    filename: 'YelpCamp/casxjgqi9oupmti23iw2',
+                    url: 'https://res.cloudinary.com/dwtslthej/image/upload/v1717354170/YelpCamp/qk9nutcguf6gnc6308vm.webp',
+                    filename: 'YelpCamp/qk9nutcguf6gnc6308vm',
                 },
                 {
-                    url: 'https://res.cloudinary.com/dwtslthej/image/upload/v1717316517/YelpCamp/xfu0tlfws4qvjeknh9l1.jpg',
-                    filename: 'YelpCamp/xfu0tlfws4qvjeknh9l1',
+                    url: 'https://res.cloudinary.com/dwtslthej/image/upload/v1717354169/YelpCamp/vmy2gj7n28wv8gkzjiav.webp',
+                    filename: 'YelpCamp/vmy2gj7n28wv8gkzjiav',
                 }
             ],
-            author: '6656f72c841078ded5dabd6c',
-            __v: 0
+            "author": "6656f72c841078ded5dabd6c",
+            "__v": 0,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-112.1401, 36.1069]
+            }
         },
         {
-            title: 'Grizzly Mule Camp',
-            price: 12,
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, fuga suscipit dolor in distinctio tempora id sit harum ipsa incidunt aperiam quam, ipsam provident quod libero exercitationem nam voluptas perferendis.',
-            location: 'Upland, California',
-            reviews: [],
-            images: [
-                {
-                    url: 'https://res.cloudinary.com/dwtslthej/image/upload/v1717316597/YelpCamp/xbrqunftutcjniim4ahl.jpg',
-                    filename: 'YelpCamp/xbrqunftutcjniim4ahl',
-                },
+            "title": "Lakeside Meadow",
+            "price": 27,
+            "description": "Beautiful lakeside views and abundant wildlife make this a favorite spot for nature lovers.",
+            "location": "Glacier National Park, Montana",
+            "reviews": [],
+            "images": [
                 {
                     url: 'https://res.cloudinary.com/dwtslthej/image/upload/v1717316598/YelpCamp/fxod0oygrpl8nsf9oyof.webp',
                     filename: 'YelpCamp/fxod0oygrpl8nsf9oyof',
+                },
+                {
+                    url: 'https://res.cloudinary.com/dwtslthej/image/upload/v1717316597/YelpCamp/xbrqunftutcjniim4ahl.jpg',
+                    filename: 'YelpCamp/xbrqunftutcjniim4ahl',
                 }
             ],
-            author: '6656f72c841078ded5dabd6c',
-            __v: 0
+            "author": "6656f72c841078ded5dabd6c",
+            "__v": 0,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-113.7870, 48.6968]
+            }
         },
         {
-            title: 'Redwood Village',
-            price: 19,
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, fuga suscipit dolor in distinctio tempora id sit harum ipsa incidunt aperiam quam, ipsam provident quod libero exercitationem nam voluptas perferendis.',
-            location: 'Palm coast, New Jersey',
-            reviews: [],
-            images: [
+            "title": "Desert Mirage",
+            "price": 15,
+            "description": "Experience the beauty of the desert and stunning night skies at Desert Mirage.",
+            "location": "Joshua Tree National Park, California",
+            "reviews": [],
+            "images": [
                 {
                     url: 'https://res.cloudinary.com/dwtslthej/image/upload/v1717316669/YelpCamp/d927pjr1gogcbtqjp8bg.jpg',
                     filename: 'YelpCamp/d927pjr1gogcbtqjp8bg',
@@ -209,10 +243,16 @@ const seedDB = async () => {
                     filename: 'YelpCamp/kruk62fivd5n4wyikqmr',
                 }
             ],
-            author: '6656f72c841078ded5dabd6c',
-            __v: 0
+            "author": "6656f72c841078ded5dabd6c",
+            "__v": 0,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-116.3131, 33.8734]
+            }
         }
-    ])
+    ];
+    
+    await Campground.insertMany(campgrounds);
 }
 
 seedDB().then(() => {
