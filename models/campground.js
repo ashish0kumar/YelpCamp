2bbfbc5a577f5020a8a5862090eb3e5e05e02e1c
Ashish Kumar <ashishkumar901336@gmail.com>
2024-06-02 17:34:58 +0530

    feat: img thumbnails in edit page for deleting

--- models/campground.js
+++ models/campground.js
@@ -2,14 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review");
 
+const imageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+imageSchema.virtual("thumbnail").get(function() {
+    return this.url.replace("/upload", "/upload/w_150");
+});
+
 const campgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [imageSchema],
     price: Number,
     description: String,
     location: String,
