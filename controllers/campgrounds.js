59a57e29f70252e3a9d887d176b8bcd87aa8bcef
Ashish Kumar <ashishkumar901336@gmail.com>
2024-06-02 16:16:47 +0530

    feat: image upload

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require("../models/campground");
+const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -11,6 +12,7 @@ module.exports.renderNewForm = (req, res) => {
 
 module.exports.createCampground = async (req, res) => {
     const campground = new Campground(req.body.campground);
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.author = req.user._id;
     await campground.save();
 
@@ -44,7 +46,19 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
+    console.log(req.body);
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    campground.images.push(...imgs);
+    await campground.save();
+
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
+    }
+
     req.flash("success", "Campground updated successfully");
     res.redirect(`/campgrounds/${campground._id}`);
 }
--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
-const axios = require("axios");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 
@@ -18,15 +17,22 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 30; i++) {
         const price = Math.floor(Math.random() * 20) + 10;
-        const res = await axios.get("https://api.unsplash.com/photos/random?client_id=tVnezep4QeEafTgUrQM2QVhBZiVpWIKwc_K0xChaj1k&collections=483251&count=1&orientation=landscape");
-        const imgURL = res.data[0].urls.raw;
         const camp = new Campground({
             author: '6656f72c841078ded5dabd6c',
             location: `${sample(cities).city}, ${sample(cities).state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: imgURL,
             description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, fuga suscipit dolor in distinctio tempora id sit harum ipsa incidunt aperiam quam, ipsam provident quod libero exercitationem nam voluptas perferendis.",
-            price
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dwtslthej/image/upload/v1717266803/YelpCamp/t5fcbzlqvuzxwofw3tco.webp',
+                    filename: 'YelpCamp/t5fcbzlqvuzxwofw3tco'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dwtslthej/image/upload/v1717266804/YelpCamp/tacas2nwq8qgnplgzlh0.jpg',
+                    filename: 'YelpCamp/tacas2nwq8qgnplgzlh0'
+                }
+            ]
         })
         await camp.save();
     }
