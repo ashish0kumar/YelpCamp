a419d1df70a8b91a82f9eee7531cc724607dfe8e
Ashish Kumar <ashishkumar901336@gmail.com>
2024-06-03 18:03:57 +0530

    feat: added navigation controls in maps

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -3,8 +3,10 @@ const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
     center: campground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 10, // starting zoom
+    zoom: 8, // starting zoom
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
